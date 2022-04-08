77070c53ec11f0e3423f28c6fa52fd7ed13eb9a7
VitorHWitt <45514663+VitorHWitt@users.noreply.github.com>
2022-04-07 23:42:02 -0300

    Java1/7 - pesquisando os top 250 filmes

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+
+        // criando cliente
+        HttpClient client = HttpClient.newHttpClient();
+        // criando request
+        HttpRequest request = HttpRequest
+                .newBuilder()
+                .uri(URI.create("https://imdb-api.com/en/API/Top250Movies/k_2beq6vho"))
+                .GET()
+                .build();
+        // Armazenando a resposta
+        HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+}
