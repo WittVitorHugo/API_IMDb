c2098b90aad306feae847bcb19781a03bdfe0e9c
VitorHWitt <45514663+VitorHWitt@users.noreply.github.com>
2022-04-09 11:15:06 -0300

    teste api key secret

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.Iterator;
 
 public class Main {
 
@@ -15,9 +14,10 @@ public class Main {
         // criando request
         HttpRequest request = HttpRequest
                 .newBuilder()
-                .uri(URI.create("https://imdb-api.com/en/API/Top250Movies/{apiKey}"))
+                .uri(URI.create("https://imdb-api.com/en/API/Top250Movies/k_2beq6vho"))
                 .GET()
                 .build();
+
         // Armazenando a resposta
         HttpResponse response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
